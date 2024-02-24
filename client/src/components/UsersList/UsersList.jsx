33056c5aec0f0f973a569980ab038381c5476306
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2024-02-24 18:35:41 +0100

    diseño

--- client/src/components/UsersList/UsersList.jsx
+++ client/src/components/UsersList/UsersList.jsx
@@ -17,13 +17,20 @@ export default function UsersList() {
         socket.off("newUser");
       };
     }, []);
+
+    function activar(e){
+      if(document.getElementsByClassName("usuario__activo").length>0){
+      document.getElementsByClassName("usuario__activo")[0].classList.remove("usuario__activo");
+      }
+      e.target.classList.add("usuario__activo")
+    }
   
     return (
-      <div className='lista px-5'>
-        <h2 className='text-white'>Lista de Usuarios:</h2>
+      <div className='lista'>
+        <h2 className='text-white px-5'>Lista de Usuarios:</h2>
         <section>
           {usuarios.map((element) => (
-            <article className='d-flex align-items-center my-3 text-white usuario' key={element.uid}>
+            <article onClick={activar} className='d-flex align-items-center my-3 text-white usuario px-5 py-3' key={element.uid}>
               <div className='usuario__img'>
                 <img src={element.foto} alt={`Foto de perfil de ${element.nick}`} />
               </div>
